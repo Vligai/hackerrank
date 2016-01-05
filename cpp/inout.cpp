de034cdba666f676a2b6673bb9b2cd3a789eca78
vligai <vligai@stevens.edu>
2016-01-05 13:49:46 -0500

    warming up

---
+++ cpp/inout.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i, j, k, result;
+    std::cin >> i  >> j  >> k;
+    result = i + j + k;
+    std::cout << result;
+    return 0;
+}
