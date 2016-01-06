89e1cfdde3c25162490bbca89a3495db8259d231
vligai <vligai@stevens.edu>
2016-01-06 12:27:32 -0500

    stringgs

---
+++ cpp/strings/stringstream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+	char ch;
+	int a;
+    vector<int> b; 
+	for (;ss >> a;) {
+		b.push_back(a);
+		ss >> ch;
+	}
+
+	return b;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
