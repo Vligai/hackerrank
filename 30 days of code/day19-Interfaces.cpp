881bc6d99001e473e0b983516654d5f30cdc5b85
vligai <vligai@stevens.edu>
2017-11-23 21:26:14 -0500

    stuff

---
+++ 30 days of code/day19-Interfaces.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+//Write your code here
+class Calculator: public AdvancedArithmetic{
+    int sum =1;
+    public:
+        int divisorSum(int num){
+            for (int i=2;i<=num;i++){
+                if (num%i==0){
+                    sum +=i;
+                }
+            }
+            return sum;
+        }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
